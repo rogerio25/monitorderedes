b3f4d733f2bcbb17077710ade2c3c8131ed72647
rogerio25 <50890831+rogerio25@users.noreply.github.com>
2019-05-23 19:28:46 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<html>
+<head><meta charset="utf-8">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<link rel="stylesheet" href="css/estilo.css">
+<title>Monitor de Redes</title>
+</head>
+<body>
+
+<h1>Monitor de Redes</h1>
+<form method="post" class="form-inline">
+<div class="row">
+
+<div class="col">
+<input type="text" placeholder="nome" name="nomecomputador" class="form-control">
+</div>
+<div class="col">
+<input type="text" placeholder="ip" class="form-control">
+</div>
+<input type="submit" class="btn btn-outline-secondary" value="salvar"/>
+
+</div>
+
+</div>
+
+
+</form>
+</body>
+</html>
